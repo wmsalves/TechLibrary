ad300f6f2bc2d5e1713e9465bd4fa8dffc7ce7cd
wemer <wemersoncontas22@gmail.com>
2025-02-27 11:14:26 -0300

    Add project files.

---
+++ TechLibrary.Api/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechLibrary.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Created();
+        }
+    }
+}
